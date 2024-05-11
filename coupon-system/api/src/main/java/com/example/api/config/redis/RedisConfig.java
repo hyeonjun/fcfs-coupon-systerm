5577b8dcbe153d3b014f48228cb8be419dbe1fb1
hyeonjun <zlel175@naver.com>
2024-05-11 16:27:55 +0900

    redisConfig refactor

--- coupon-system/api/src/main/java/com/example/api/config/redis/RedisConfig.java
+++ coupon-system/api/src/main/java/com/example/api/config/redis/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.example.api.config;
+package com.example.api.config.redis;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
