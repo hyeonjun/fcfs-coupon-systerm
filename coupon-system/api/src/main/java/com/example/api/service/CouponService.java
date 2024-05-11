e81cea08cceb5f30aeb92aa4e28cae64596ace22
hyeonjun <zlel175@naver.com>
2024-05-11 14:54:40 +0900

    coupon count - redis

--- coupon-system/api/src/main/java/com/example/api/service/CouponService.java
+++ coupon-system/api/src/main/java/com/example/api/service/CouponService.java
@@ -1,30 +1,30 @@
 package com.example.api.service;
 
 import com.example.api.domain.Coupon;
+import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CouponService {
 
   private final CouponRepository couponRepository;
+  private final CouponCountRepository couponCountRepository;
 
-  @Transactional
-  public Optional<Coupon> createCoupon(Long userId) {
-    long count = couponRepository.count();
+//  @Transactional
+  public Coupon createCoupon(Long userId) {
+    long count = couponCountRepository.increment();
 
     if (count > 100) {
-      return Optional.empty();
+      return null;
     }
 
     Coupon coupon = Coupon.builder()
       .userId(userId).build();
-    return Optional.of(couponRepository.save(coupon));
+    return couponRepository.save(coupon);
   }
+
 
 }
