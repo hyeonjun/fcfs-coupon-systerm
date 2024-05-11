6ccd5867c3bd70c1edba4736c1323a9b3578fe62
hyeonjun <zlel175@naver.com>
2024-05-11 14:54:08 +0900

    redis config

---
+++ coupon-system/api/src/main/java/com/example/api/config/RedisConfig.java
@@ -0,0 +1,52 @@
+package com.example.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class RedisConfig {
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    return new LettuceConnectionFactory();
+  }
+
+  @Bean
+  public RedisTemplate<?, ?> redisTemplate() {
+    RedisTemplate<?, ?> template = new RedisTemplate<>();
+    template.setConnectionFactory(redisConnectionFactory());
+    template.setKeySerializer(new StringRedisSerializer());
+    template.setValueSerializer(new StringRedisSerializer());
+    template.setEnableTransactionSupport(true); // 설정 필요
+    return template;
+  }
+
+  /* JPA 의 JpaTransactionManager 를 사용하는 방법 */
+
+  @Bean
+  public PlatformTransactionManager transactionManager() {
+    return new JpaTransactionManager();
+  }
+
+  /* JDBC 의 DataSourceTransactionManager 를 사용하는 방법
+  @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+   */
+
+}
