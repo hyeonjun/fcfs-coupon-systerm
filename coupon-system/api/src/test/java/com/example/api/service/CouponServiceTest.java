660baaae11d8eed496108e5e4c232c1b1e6be5a2
hyeonjun <zlel175@naver.com>
2024-05-18 16:03:50 +0900

    redis set 을 활용하여 1인 1쿠폰 발급되는지 테스트

--- coupon-system/api/src/test/java/com/example/api/service/CouponServiceTest.java
+++ coupon-system/api/src/test/java/com/example/api/service/CouponServiceTest.java
@@ -17,6 +17,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import static com.example.api.service.TestType.KAFKA_WITH_REDIS_SET;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -50,7 +51,7 @@ class CouponServiceTest {
   }
 
   @ParameterizedTest
-  @EnumSource(value = TestType.class, names = {"REDIS", "KAFKA"})
+  @EnumSource(value = TestType.class, names = {"REDIS", "KAFKA",})
   void 동시에_1000개의_요청(TestType type) throws InterruptedException {
     int threadCount = 1000;
     // ExecutorService: 병령 작업을 간단하게 할 수 있게 도와주는 Java API
@@ -82,6 +83,38 @@ class CouponServiceTest {
     assertEquals(100L, count);
   }
 
+  @Test
+  void 하나의_유저가_1000번_요청() throws InterruptedException {
+    int threadCount = 1000;
+    // ExecutorService: 병령 작업을 간단하게 할 수 있게 도와주는 Java API
+    ExecutorService executorService = Executors.newFixedThreadPool(32);
+    // 다른 스레드에서 수행하는 작업을 기다리도록 도와주는 클래스
+    CountDownLatch latch = new CountDownLatch(threadCount);
+
+    Long userId = 1L;
+    for (int i=0; i<threadCount; i++) {
+      executorService.submit(() -> {
+        try {
+          execute(userId, KAFKA_WITH_REDIS_SET);
+        } catch (Exception e) {
+          log.error(e.getMessage());
+        } finally {
+          latch.countDown();;
+        }
+      });
+    }
+
+    latch.await();
+
+    // kafka consumer 가 데이터를 받아 실제 db에 저장하기까지 시간이 걸리기 때문에
+    // 넉넉하게 10초 정도 기다린 후 개수를 확인한다.
+    Thread.sleep(10000L);
+
+    long count = couponRepository.count();
+
+    assertEquals(1L, count);
+  }
+
   void execute(long userId, TestType type) {
     switch (type) {
       case REDIS:
@@ -89,6 +122,9 @@ class CouponServiceTest {
         break;
       case KAFKA:
         couponService.createCouponForKafka(userId);
+        break;
+      case KAFKA_WITH_REDIS_SET:
+        couponService.createCouponForKafkaUsingRedisSet(userId);
         break;
       default:
         throw new RuntimeException("not supported type");
@@ -98,5 +134,6 @@ class CouponServiceTest {
 
 enum TestType {
   REDIS,
-  KAFKA
+  KAFKA,
+  KAFKA_WITH_REDIS_SET
 }
